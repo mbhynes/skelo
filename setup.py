4dd78a29510071cb4e3e7a2eb413f82f7700fecb
Michael Hynes <mike@hynesbox.local>
2022-08-19 17:34:02 -0400

    Converted toml to setup.py to ensure dependencies install

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="skelo",
+  version="0.1.2",
+  author="Michael B Hynes",
+  author_email="mike.hynes.rhymes@gmail.com",
+  description="A scikit-learn interface to the Elo and Glicko2 rating systems",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://skelo.readthedocs.io/en/latest/",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+  ],
+  python_requires='>=3',
+  install_requires=[
+    "numpy",
+    "sklearn",
+    "pandas",
+    "glicko2>=2",
+  ],
+)
--- skelo/__init__.py
+++ skelo/__init__.py
@@ -1,1 +1,4 @@
 __version__ = "0.1.0"
+
+from skelo import model
+from skelo import utils
