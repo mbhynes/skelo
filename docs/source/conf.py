aec5d4f834eb630efc9ba9b29e11e40a2c87b8fd
Michael Hynes <mike.hynes@protonmail.com>
2022-08-20 16:05:22 -0400

    Removed failing import from sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'skelo'
 copyright = '2022, Michael B Hynes'
 author = 'Michael B Hynes'
-from skelo import __version__ as release
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
